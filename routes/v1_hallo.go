a4d59af1ddccc9ee1b0b744a1a7085680559b7c6
mehmet ali büber <mehmetali_buber@hotmail.com>
2021-09-11 11:28:46 +0300

    postHall done

--- routes/v1_hallo.go
+++ routes/v1_hallo.go
@@ -1,6 +1,16 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+//  "request":{"name":"Vitali","company":"LotInternet","task":"make_cofee"}}
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+)
+
+type HalloOrder struct {
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Task    string `json:"task"`
+}
 
 func GetHallo(c *gin.Context) {
 
@@ -11,7 +21,13 @@ func GetHallo(c *gin.Context) {
 
 func PostHallo(c *gin.Context) {
 
+	var h HalloOrder
+	if err := c.ShouldBindBodyWith(&h, binding.JSON); err != nil {
+		c.JSON(404, gin.H{
+			"message": "something went wrong",
+		})
+	}
 	c.JSON(200, gin.H{
-		"message": "post",
+		"result": "Hallo," + h.Name + " from " + h.Company + " Please do task: " + h.Task,
 	})
 }
